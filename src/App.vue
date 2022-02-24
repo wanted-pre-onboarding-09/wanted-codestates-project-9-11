b25eb27b12ed89be4c872a2338bd7e942c3bd2d2
jisoo <devjisoolee@gmail.com>
2022-02-24 19:48:35 +0900

    feat: Header layour

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderPage from './components/HeaderPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderPage
   }
-}
+};
 </script>
 
 <style>
