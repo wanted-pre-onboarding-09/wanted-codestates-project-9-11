1b8b75ca3b81132837e5897ef60fa13a7d0b345f
ysh2987 <ysh2987@naver.com>
2022-02-24 19:00:43 +0900

    Fix: Mock data

--- src/mock/index.js
+++ src/mock/index.js
@@ -8,25 +8,34 @@ const user = {
 
 const company = {
   samsung: {
-    aggressive: 10,
-    confident: 9,
-    responsible: 9,
-    indivisual: 5,
-    horizontal: 3,
+    name: "삼성",
+    score: {
+      aggressive: 10,
+      confident: 9,
+      responsible: 9,
+      indivisual: 5,
+      horizontal: 3,
+    },
   },
   kakao: {
-    aggressive: 6,
-    confident: 7,
-    responsible: 7,
-    indivisual: 8,
-    horizontal: 9,
+    name: "카카오",
+    score: {
+      aggressive: 6,
+      confident: 7,
+      responsible: 7,
+      indivisual: 8,
+      horizontal: 9,
+    },
   },
   lg: {
-    aggressive: 7,
-    confident: 7,
-    responsible: 7,
-    indivisual: 7,
-    horizontal: 7,
+    name: "LG",
+    score: {
+      aggressive: 7,
+      confident: 7,
+      responsible: 7,
+      indivisual: 7,
+      horizontal: 7,
+    },
   },
 };
 
