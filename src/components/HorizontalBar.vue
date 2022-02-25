5633c8c9bb6d6c5acf6c4b02939446b7019c3938
y-solb <ysolb99@gmail.com>
2022-02-25 13:28:24 +0900

    Feat: Add data to HorizontalVar

--- src/components/HorizontalBar.vue
+++ src/components/HorizontalBar.vue
@@ -3,47 +3,107 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 import { BarChart } from "vue-chart-3";
 import { Chart, registerables } from "chart.js";
-import { ref } from "vue";
 
 Chart.register(...registerables);
 
 export default defineComponent({
+  props: {
+    users: Object,
+    companies: Object,
+  },
   name: "HorizontalBar",
   components: { BarChart },
-  setup() {
+
+  setup(props) {
     const barRef = ref();
     const options = ref({
       responsive: true,
       maintainerAspectRatio: false,
       indexAxis: "y",
+      scales: {
+        y: {
+          ticks: {
+            display: false,
+          },
+          grid: {
+            borderColor: "white",
+            display: false,
+          },
+        },
+        x: {
+          min: -10,
+          max: 10,
+          ticks: {
+            display: false,
+          },
+          grid: {
+            borderColor: "white",
+            display: false,
+          },
+        },
+      },
       plugins: {
         legend: {
           display: false,
         },
       },
     });
+
+    const getUser = (users) => {
+      let usersData = Object.values(users);
+      usersData = usersData.map((user) => {
+        if (user > 5) {
+          return user * -1;
+        } else {
+          return user;
+        }
+      });
+      //console.log(usersData);
+      return usersData;
+    };
+
+    const getCompany = (users, companies) => {
+      let companiesData = [0, 0, 0, 0, 0];
+      let usersData = Object.values(users);
+      for (let i = 0; i < 5; i++) {
+        if (usersData[i] > 5) {
+          companiesData[i] = companies[i] * -1;
+        } else {
+          companiesData[i] = companies[i];
+        }
+      }
+      //   console.log(companiesData, "com");
+      return companiesData;
+    };
+
     const testData = {
       labels: ["", "", "", "", ""],
       datasets: [
         {
           label: "one",
-          data: [-2, -4, -6, 4, -5],
-
+          data: getUser(props.users),
           barThickness: 6,
           backgroundColor: "#6E3CF9",
         },
         {
           label: "two",
-          data: [-8, -6, -4, 6, -5],
+          data: getCompany(props.users, props.companies),
           barThickness: 6,
           backgroundColor: "#FFC24A",
         },
       ],
     };
+    console.log(testData);
 
+    //  console.log(getData(props.users, props.companies));
+    onMounted(() => {
+      // console.log(props.companies, "dd");
+      // const cc = getR(props.users, props.companies);
+      // console.log(cc);
+    });
     return { testData, options, barRef };
   },
 });
